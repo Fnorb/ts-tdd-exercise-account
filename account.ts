ff4e6464b3c8e206f9e7b2e418492d36e1160aaa
Fnorb <azaca@yahoo.com>
2025-05-27 14:23:22 +0200

    feat(account): implement withdrawal limit related features

--- account.ts
+++ account.ts
@@ -1,5 +1,8 @@
 export class Account {
   private balance: number = 0;
+  private transactionHistory: { date: Date; amount: number }[] = [];
+  private dailyWithdrawalLimit: number = 1000;
+  private date: Date = new Date();
 
   getBalance(): number {
     return this.balance;
@@ -10,6 +13,7 @@ export class Account {
       throw new Error("Deposit must be greater than zero");
     }
     this.balance += amount;
+    this.transactionHistory.push({ date: this.date, amount: amount });
     return this.balance;
   }
 
@@ -20,7 +24,26 @@ export class Account {
     if (this.balance - amount < 0) {
       throw new Error("Negative balance not allowed");
     }
+    if (
+      this.getWithdrawalSumFor(this.date) + amount >
+      this.dailyWithdrawalLimit
+    ) {
+      throw new Error("Daily limit reached");
+    }
     this.balance -= amount;
+    this.transactionHistory.push({ date: this.date, amount: amount * -1 });
     return this.balance;
+  }
+
+  getWithdrawalSumFor(date: Date): number {
+    return this.transactionHistory
+      .filter(
+        (t) => t.amount < 0 && t.date.toDateString() === date.toDateString()
+      )
+      .reduce((sum, t) => sum + Math.abs(t.amount), 0);
+  }
+
+  setDateForTest(date: Date): void {
+    this.date = date;
   }
 }
